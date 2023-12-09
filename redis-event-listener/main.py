b8210203687b033f532905b8998745bd1aec5b66
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-12-08 23:42:00 -0300

    fix listen

--- redis-event-listener/main.py
+++ redis-event-listener/main.py
@@ -1,14 +1,13 @@
 import redis
 
-
-
 redis_conn = redis.Redis('redis-leader', 6379)
-redis_subscriber = redis_conn.pubsub().psubscribe('page:*:likes')
+redis_subscriber = redis_conn.pubsub()
+redis_subscriber.psubscribe('page:*:likes')
 
 for message in redis_subscriber.listen():
-
-    print('aaaa')
+    with open('log.txt', 'a') as f:
+        f.write(str(message))
     if message['type'] == 'pmessage':
         page_id = message['channel'].split(':')[1]
         likes_count = redis_conn.get(f'page:{page_id}:likes')
-        print(f'Page {page_id} has {likes_count} likes')
+        print(f'Page {page_id} has {likes_count} likes', flush=True)
