22ebbb0aa4fd1c7f3d76fc90b492b9ca4085c0aa
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-12-08 23:59:01 -0300

    subscribe to channel

--- redis-event-listener/main.py
+++ redis-event-listener/main.py
@@ -5,9 +5,13 @@ redis_subscriber = redis_conn.pubsub()
 redis_subscriber.psubscribe('page:*:likes')
 
 for message in redis_subscriber.listen():
-    with open('log.txt', 'a') as f:
-        f.write(str(message))
     if message['type'] == 'pmessage':
-        page_id = message['channel'].split(':')[1]
+        channel = message['channel'].decode('utf-8')
+        page_id = channel.split(':')[1]
+
+        print(message['data'], flush=True)
+
         likes_count = redis_conn.get(f'page:{page_id}:likes')
+        if likes_count:
+            likes_count = likes_count.decode('utf-8')
         print(f'Page {page_id} has {likes_count} likes', flush=True)
